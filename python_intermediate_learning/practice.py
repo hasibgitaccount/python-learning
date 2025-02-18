a356d27aeffd89521ea42a3c3ca2134e76351e32
Hasib <mdhasib0296@gmail.com>
2025-02-18 12:32:52 +0600

    finished tuple and list

---
+++ python_intermediate_learning/practice.py
@@ -0,0 +1,21 @@
+a= 4
+b = 4
+
+operator1 =('user wants addition',)
+operator2 =('user wants substraction',)
+operator3 =('user wants multiplication',)
+operator4 =('user wants division',)
+
+if 'user wants addition' in operator1: 
+    print('a + b is equal to', a + b)
+
+if 'user wants substraction' in operator2:
+    print('a - b is equal to', a - b)
+
+if 'user wants multiplication' in operator3:
+    print('a * b is equal to', a * b)
+
+if 'user wants division' in operator4:
+    print('a / b is equal to', a / b)
+
+
