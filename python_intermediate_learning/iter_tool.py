d2cdb3e8514ebdd2f3c240359cd2ba09cf29f530
Hasib <mdhasib0296@gmail.com>
2025-02-21 16:23:43 +0600

    finished

--- python_intermediate_learning/iter_tool.py
+++ python_intermediate_learning/iter_tool.py
@@ -11,49 +11,95 @@
 # PRODUCT
 
 # first of all we have to import it
+from itertools import groupby
+import operator
+from itertools import accumulate
+from itertools import combinations, combinations_with_replacement
+from itertools import combinations
+from itertools import permutations
 from itertools import product
-a = [1,2]
-b = [3,4]
-p = product(a,b) # here the product will compute the cartesian product of the input iterables.
-'''print(list(p))'''
+a = [1, 2]
+b = [3, 4]
+# here the product will compute the cartesian product of the input iterables.
+p = product(a, b)
+print(list(p))
 # we can also define a number of repition
 b = [3]
-p = product(a,b, repeat = 2)
-'''print(list(p))'''
+p = product(a, b, repeat=2)
+print(list(p))
 
 # PERMUTATIONS
 
 # permutations will return all possible orderings of an input
-from itertools import permutations
-a = [1,2,3]
+a = [1, 2, 3]
 p = permutations(a)
-'''print(list(p))'''
+print(list(p))
 # we can also specify the length of the permutations as a second arguments. so if we want to have shorter or longer permutations then we just specify the arguments.
 p = permutations(a, 2)
-'''print(list(p))'''
+print(list(p))
 
 # COMBINATIONS
 # the combinations function will make all possible combinations with a specified length.
-from itertools import combinations
-a = [1,2,3,4]
+a = [1, 2, 3, 4]
 c = combinations(a, 2)
-'''print(list(c))'''
+print(list(c))
 # NOTE: here we dont have combinations of the smae arguments meaning there is no repition. if we want that then we will use combinations_with_repititions function.
-from itertools import combinations, combinations_with_replacement
 c_wr = combinations_with_replacement(a, 2)
-'''print(list(c_wr))'''
+print(list(c_wr))
 
 # ACCUMULATE
 # the accumulate function makes and iterator that returns accumulated sums or any other function that it will as input.
-from itertools import accumulate
-import operator
-a = [1,2,3,4]
+a = [1, 2, 3, 4]
 acc = accumulate(a)
-'''print(list(acc))''' # by default it will compute the sums.
+print(list(acc))  # by default it will compute the sums.
 # it can also multiply the elements. then we need to give an argument.
-ac = accumulate(a, func = operator.mul)
-'''print(list(ac))'''
+ac = accumulate(a, func=operator.mul)
+print(list(ac))
 # max
-a = [1,2,5,3,4]
-acc_max = accumulate(a, func= max)
-'''print(list(acc_max))'''
+a = [1, 2, 5, 3, 4]
+acc_max = accumulate(a, func=max)
+print(list(acc_max))
+
+# GROUPBY
+# the groupby function makes an iterator that returns keys and groups from an iterable.
+
+
+def smaller_than_3(x):
+    return x < 3
+
+
+a = [1, 2, 3, 4]
+# we also have to give a key, we can define it as a function also.
+g = groupby(a, key=smaller_than_3)
+for key, value in g:
+    print(key, list(value))
+# we can also use lambda function here.
+# LAMBDA.
+# lambda are small one line function that can have an input and will do some expression and then will return an output.
+# we also have to give a key, we can define it as a function also.
+g = groupby(a, key=lambda x: x < 4)
+for key, value in g:
+    print(key, list(value))
+
+persons = [{'name': 'tom', 'age': 25}, {'name': 'chinku', 'age': 25},
+           {'name': 'pinku', 'age': 27}, {'name': 'shingshing', 'age': 28}]
+g = groupby(persons, key=lambda x: x['age'])
+for key, value in g:
+    print(key, list(value))
+
+# INFINITE ITERATORS
+# COUNT
+from itertools import count, cycle, repeat
+for i in count(10):
+    print(i)
+    if i == 15:
+        break
+# CYCLE
+a = [1,2,3]
+for i in cycle(a):
+    '''print(i)''' # dont ever run it . it is infinite
+
+# REPEAT
+for i in repeat(1):
+    '''print(i)''' # dont run it
+
