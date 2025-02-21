778e38ce255f57a036da3649b57d60f82d4eaab5
Hasib <mdhasib0296@gmail.com>
2025-02-21 14:47:17 +0600

    finished third part

--- python_intermediate_learning/iter_tool.py
+++ python_intermediate_learning/iter_tool.py
@@ -31,3 +31,19 @@ p = permutations(a)
 # we can also specify the length of the permutations as a second arguments. so if we want to have shorter or longer permutations then we just specify the arguments.
 p = permutations(a, 2)
 '''print(list(p))'''
+
+# COMBINATIONS
+# the combinations function will make all possible combinations with a specified length.
+from itertools import combinations
+a = [1,2,3,4]
+c = combinations(a, 2)
+'''print(list(c))'''
+# NOTE: here we dont have combinations of the smae arguments meaning there is no repition. if we want that then we will use combinations_with_repititions function.
+from itertools import combinations, combinations_with_replacement
+c_wr = combinations_with_replacement(a, 2)
+'''print(list(c_wr))'''
+
+# ACCUMULATE
+#
+from itertools import accumulate
+
