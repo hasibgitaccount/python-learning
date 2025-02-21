c2168b30713b151fe334d3f2a41cd3cb45e9c408
Hasib <mdhasib0296@gmail.com>
2025-02-21 11:57:00 +0600

    finished everything

--- python_intermediate_learning/collection.py
+++ python_intermediate_learning/collection.py
@@ -13,19 +13,19 @@ from collections import Counter
 
 example = 'aaaaaabbbbbbcccccc'
 my_counter = Counter(example)
-'''print(my_counter) '''# so it basically counted how many times a element is in a variable.
+print(my_counter) # so it basically counted how many times a element is in a variable.
 # like any other normal dictionary we can look at the items , keys, values etc.
-'''print(my_counter.items())
+print(my_counter.items())
 print(my_counter.keys())
 print(my_counter.values())
 
-'''
-'''print(my_counter.most_common(2)) # it will show you upto your last number of elments . if 2 then first 2 elements. btw it is wrapped by a list with a tuple inside. to reach more inside we can use indexing.
+
+print(my_counter.most_common(2)) # it will show you upto your last number of elments . if 2 then first 2 elements. btw it is wrapped by a list with a tuple inside. to reach more inside we can use indexing.
 print(my_counter.most_common(1)[0])
 print(my_counter.most_common(1)[0][0]) # here a is the most common element in our string.
-print(my_counter.most_common(1)[0][1])'''
+print(my_counter.most_common(1)[0][1])
 # we can also convert all the elelment inside to a list. using list element method.
-'''print(list(my_counter.elements()))'''
+print(list(my_counter.elements()))
 
 # NAMEDTUPLE
 # of course first of all we have to import it.
@@ -36,9 +36,9 @@ point = namedtuple('point', 'x,y') # here as the first argument we will give the
 # so what happened there was it created a class called point with the fields x and y.
 # so now i can create this point.
 pt = point(1, -4)
-'''print(pt)
+print(pt)
 # i can also access the fields.
-print(pt.x, pt.y)'''
+print(pt.x, pt.y)
 
 # OrderedDict
 # OrderedDict is like a regular dictionary but they remember the order the items were inserted. they have lose importance since normal dict has also function now.
@@ -47,13 +47,13 @@ order_dict = OrderedDict()
 order_dict['b'] = 3
 order_dict['c'] = 4
 order_dict['a'] = 2
-'''print(order_dict) # as you can see it can remember its order .
+print(order_dict) # as you can see it can remember its order .
 
 normal_dict = {}
 normal_dict['b'] = 3
 normal_dict['c'] = 4
 normal_dict['a'] = 2
-print(normal_dict) '''# normal dictionary can also use so thats usecase has now become obsolete.
+print(normal_dict) # normal dictionary can also use so thats usecase has now become obsolete.
 
 # defaultdict
 # the defaultdict is also very similar to normal dict. the only difference is that it will have a default value if the key has not been set yet.
@@ -61,9 +61,39 @@ from collections import defaultdict
 d_dict = defaultdict(int) # here as a argument we will give a default type
 d_dict['a'] = 1.0
 d_dict['b'] = 2
-'''print(d_dict)
-print(d_dict['c'])''' # here since 'c' key doesnt have a int value so it took the default int value 0. if it was float then it would give 0.0, list will give a empty list
+print(d_dict)
+print(d_dict['c']) # here since 'c' key doesnt have a int value so it took the default int value 0. if it was float then it would give 0.0, list will give a empty list
 
 # deque
-
+# the deque is a double ended queue and it can be used to add and remove elements from both ends. and both are implemented in a way that this will be very efficient.
 from collections import deque
+d = deque() # lets create a deque 
+
+d.append(1) # now we can append items like with a list
+d.append(2)
+print(d)
+d.appendleft(3)# it will add elements in the left
+print(d)
+# we can also remove it
+d.pop()
+print(d)
+d.popleft()
+print(d)
+# we can also clear all elements
+d.clear()
+print(d)
+# we can also add multiple elements in right or left
+d.extend([4,5,6])
+print(d)
+d.extendleft([7,8,9])
+print(d)
+# we can also rotate all elements to right a given amount of time
+d.rotate(1)
+print(d)
+d.rotate(2)
+print(d)
+# if we want to rotate to left then use (-) symbol
+d.rotate(-1)
+print(d)
+d.rotate(-2)
+print(d)
