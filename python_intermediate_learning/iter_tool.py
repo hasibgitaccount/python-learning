2f427e52484e9a43a5c4ef04a013241ecb5b19a1
Hasib <mdhasib0296@gmail.com>
2025-02-21 13:49:50 +0600

    finished second part

--- python_intermediate_learning/iter_tool.py
+++ python_intermediate_learning/iter_tool.py
@@ -8,6 +8,8 @@
 5. groupby
 6. infinite iterators
 '''
+# PRODUCT
+
 # first of all we have to import it
 from itertools import product
 a = [1,2]
@@ -17,4 +19,15 @@ p = product(a,b) # here the product will compute the cartesian product of the in
 # we can also define a number of repition
 b = [3]
 p = product(a,b, repeat = 2)
+'''print(list(p))'''
+
+# PERMUTATIONS
+
+# permutations will return all possible orderings of an input
+from itertools import permutations
+a = [1,2,3]
+p = permutations(a)
+'''print(list(p))'''
+# we can also specify the length of the permutations as a second arguments. so if we want to have shorter or longer permutations then we just specify the arguments.
+p = permutations(a, 2)
 '''print(list(p))'''
