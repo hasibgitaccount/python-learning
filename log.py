992f318b82f6d047eff8de671889e26265c32e4c
Hasib <mdhasib0296@gmail.com>
2025-02-22 17:33:29 +0600

    did some modifying

---
+++ log.py
@@ -0,0 +1,5 @@
+# python already comes with a powerful build in logging module. so you can quickly add logging to your application by simply saying import logging.
+import logging
+# in this course we will have a quick look at the different block level, the different configuration options, how to log in different modules, how to use different lock handlers, how to capture stack traces in your log and how to use rotating file handler.
+
+# after importing logging we can import five different log levels.
