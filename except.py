6a882ee366e7e2b7585acb4c4805ed4a2715a030
Hasib <mdhasib0296@gmail.com>
2025-02-22 14:49:38 +0600

    did more coding

--- except.py
+++ except.py
@@ -1,1 +1,6 @@
 # a python program terminates as soon as it encounters an error and an error can be either a syntax error or an exception. now we will understand the difference between syntax error and exception. the most common built-in exceptions. how to raise and handle exceptions and how to define exceptions.
+
+# SYNTAX ERROR
+# a syntax error occurs when the parser detects a syntactically incorrect statement.
+# a = 5 print(a) # syntax error
+
