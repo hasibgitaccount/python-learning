1195e26b2bdb97bd44157ecb871c8fe552623326
Hasib <mdhasib0296@gmail.com>
2025-03-07 08:35:43 +0600

    finished with_explanation.py file coding, will use other file

---
+++ object_oriented_programming(OOP)/child@01.py
@@ -0,0 +1,21 @@
+from parent import Item
+
+class phone(Item):
+    all = []
+
+    def __init__(self, name: str, price: float, quantity = 0, broken_phones = 0):
+
+        # call to super function to have access to all the attributes / methods.
+        super().__init__(
+            name, price, quantity
+        )
+
+        # run validations to the recieve arguments.
+        assert broken_phones >= 0, f"broken_phones{quantity} is less than zero"
+        
+        # assign to self object.
+        self.broken_phones = broken_phones
+
+
+        # actions to execute
+        phone.all.append(self)
---
+++ object_oriented_programming(OOP)/parent.py
@@ -0,0 +1,61 @@
+import csv
+
+class Item:
+    all = []
+    pay_rate = 0.8 # pay rate after 20% discount.
+
+
+    def __init__(self, name: str, price: float, quantity = 0):
+
+        # run validations to the recieve arguments.
+        assert price >= 0, f"price{price} is less than zero" # it will help us understand the issue
+        assert quantity >= 0, f"quantity{quantity} is less than zero"
+        
+        # assign to self object.
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+
+        # actions to execute
+        Item.all.append(self)
+
+
+    def calculate_total_price(self):  
+        return self.price * self.quantity 
+
+
+    # now lets create a method to apply the discount.
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('object_oriented_programming(OOP)/items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+
+        for item in items:
+            '''print(item)'''
+            Item(
+                name= item.get('name'),
+                price= float(item.get('price')),
+                quantity= float(item.get('quantity'))
+            )
+
+
+
+    @staticmethod
+    def is_int(num):
+        if isinstance(num, float):
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}','{self.price}', {self.quantity})"
