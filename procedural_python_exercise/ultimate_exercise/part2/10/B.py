206e1dd6ca44c8e6a68e53c121ac9910a8ed5760
Hasib <mdhasib0296@gmail.com>
2025-03-11 11:15:55 +0600

    finished 10 of part2

--- procedural_python_exercise/ultimate_exercise/part2/10/B.py
+++ procedural_python_exercise/ultimate_exercise/part2/10/B.py
@@ -1,1 +1,13 @@
 # Create a function that sorts a list of dictionaries by a specific key (e.g., sort students by grades).
+def sort_dict_list(list_of_dicts, sort_key, reverse = False):
+    sorted_list = sorted(list_of_dicts, key=lambda x: x[sort_key], reverse=reverse)
+    return sorted_list
+
+students = [
+    {"name": "Jerry", "grade": 80},
+    {"name": "Spike", "grade": 85},
+    {"name": "Tom", "grade": 90}
+]
+
+print(sort_dict_list(students, 'grade', reverse=False))
+print(sort_dict_list(students, 'grade', reverse=True))
